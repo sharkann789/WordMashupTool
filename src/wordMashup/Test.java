0a4ddea08f0f7c59afc6cf5763b2cd977d3176e3
Sharath Kannan <skannan1@terpmail.umd.edu>
2022-11-08 18:20:58 -0500

    removed profanity in one comment

--- src/wordMashup/Test.java
+++ src/wordMashup/Test.java
@@ -6,7 +6,7 @@ import wordMashup.wordMashupTool;
 // Run command (must be in default package): javac wordMashup/wordMashupTool.java Test.java
 
 public class Test {
-    //make a bunch of random shit and run it on this!
+    //make a bunch of random stuff and run it on this!
     public static void main(String[] args){
 	    wordMashupTool test = new wordMashupTool("lamborghini","ferrari");
         System.out.println(test.getAllNames());
